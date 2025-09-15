f4e7115ac2405406516e21d8495006506e424528
vicente410 <brunottvicente@gmail.com>
2025-09-15 20:03:28 +0000

    Implemented Applicative for Parser and stringP

--- parcom-hs/Main.hs
+++ parcom-hs/Main.hs
@@ -9,10 +9,18 @@ second f (a, b) = (a, f b)
 instance Functor Parser where
   fmap f (Parser p) = Parser (fmap (second f) . p )
 
+instance Applicative Parser where
+  pure x = Parser (\input -> Just (input, x))
+  (Parser p1) <*> (Parser p2) = Parser
+    (\input -> (p1 input >>= (\(rest, f) -> second f <$> p2 rest)))
+
 charP :: Char -> Parser Char
 charP c = Parser f
   where f (x:xs) | c == x = Just (xs, x)
         f _ = Nothing
 
+stringP :: String -> Parser String
+stringP s = sequenceA $ map charP s
+
 main :: IO ()
-main = putStrLn $ show $ runParser (ord <$> charP 'H') "Hello World"
+main = putStrLn $ show $ runParser (stringP "Hello") "Hello World"
